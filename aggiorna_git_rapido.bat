3896595f84d896df9c6350e7ce8cbc9f1355b01f
Marco <marco@ecosped.it>
2026-01-02 17:27:02 +0100

    Aggiornamento sito

---
+++ aggiorna_git_rapido.bat
@@ -0,0 +1,30 @@
+@echo off
+REM === CONFIGURAZIONE ===
+SET PROJECT_DIR=D:\mio sito
+SET COMMIT_MSG=Aggiornamento sito
+SET BRANCH=main
+
+echo.
+echo Spostamento nella cartella del progetto...
+cd /d "%PROJECT_DIR%"
+
+IF NOT EXIST ".git" (
+    echo Inizializzazione repository Git...
+    git init
+)
+
+echo Aggiunta file del progetto...
+git add .
+
+echo Creazione commit...
+git commit -m "%COMMIT_MSG%"
+
+echo Impostazione branch corretto...
+git branch -M %BRANCH%
+
+echo Invio al repository GitHub...
+git push -u origin %BRANCH%
+
+echo.
+echo Operazione completata.
+pause
